a504c531d03e6be677bd961caf175bb7e434a354
victorsanchez7845 <59267510+victorsanchez7845@users.noreply.github.com>
2026-05-23 11:05:49 -0600

    Enable HTTPS in production
    Force HTTPS scheme in production environment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 // MODELS
 use App\Models\OperatorFee;
@@ -28,8 +29,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Forzar HTTPS en producción
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        // Bootstrap pagination
         Paginator::useBootstrap();
+
+        // Observers
         OperatorFee::observe(OperatorFeeObserver::class);
         ReservationsItem::observe(ReservationItemObserver::class);
     }
