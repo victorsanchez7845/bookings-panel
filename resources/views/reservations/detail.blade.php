48f9df4bd1679e8d63801bf73c3d950e04071a28
victorsanchez7845 <59267510+victorsanchez7845@users.noreply.github.com>
2026-07-29 15:52:42 -0600

    Hide CCform Llegada tab temporarily
    Temporarily hide the CCform Llegada tab and its content.

--- resources/views/reservations/detail.blade.php
+++ resources/views/reservations/detail.blade.php
@@ -523,7 +523,8 @@
                     @endif
 
                     @if ($data['transfer_types']['has_arrival'] && $data['status'] != "CANCELLED")
-                        <li class="nav-item">
+                        {{-- Oculto temporalmente: CCform Llegada --}}
+                        <li class="nav-item d-none">
                             <a class="nav-link" href="#icon-tab-6" data-bs-toggle="tab" role="tab" onclick="searchOne()">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
                                 CCform Llegada
@@ -1083,7 +1084,7 @@
                             @endif
                         </div>
                     @endif
-                    <div class="tab-pane" id="icon-tab-6" role="tabpanel">
+                    <div class="tab-pane d-none" id="icon-tab-6" role="tabpanel">
                         <div id="iframeOneContainer"></div>
                     </div>
                     <div class="tab-pane" id="icon-tab-7" role="tabpanel">
